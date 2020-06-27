6b04e2f235b61094b117b42a3aba329527e3f0b6
فوز فارس فه <66726084+fouzalmutairi@users.noreply.github.com>
2020-06-27 21:17:56 +0300

    تم

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,28 +1,45 @@
 // السؤال الأول
 // 1
+const name="fouz";
 
 // 2
+let age="16";
 
 // 3 and 4
+console.log("انا"+name+"وعمري"+age);
+
+
 
 // 5 and 6
+console.log (age*3)
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let myhobbies =["basketball","swimming","running","reading"]
+
+
 
 // 2
+myhobbies.push(age);
 
 // 3
+myhobbies.pop();
 
 // 4
+let web={
+    track:"web",
+    languge:"js",
+    ta: ["moudhi","rahaf","nancy","menawer"],
+}
 
 // 5
-
 // 6
+console.log(web.track);
 
 // 7
+console. long(web.ta[0]);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
